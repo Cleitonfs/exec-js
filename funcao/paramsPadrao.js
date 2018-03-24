0f466009ff742a815ff86f99a12b85c52f42e624
Genilton Cleiton <cleitonfs@yahoo.com>
2018-03-24 09:50:33 -0300

    params padrao com es2015

--- funcao/paramsPadrao.js
+++ funcao/paramsPadrao.js
@@ -22,4 +22,11 @@ console.log(soma2())
 console.log(soma2(2,2,2))
 console.log(soma2(0,0,0))
 
-
+function soma2(a=1, b=1, c=1){
+    
+    return a + b + c
+}
+console.log(soma2(10, 5))
+console.log(soma2())
+console.log(soma2(2,2,2))
+console.log(soma2(0,0,0))
