782c532ee3d4169229acd9e4f147a430911d846a
Genilton Cleiton <cleitonfs@yahoo.com>
2018-03-24 09:27:13 -0300

    updates

--- funcao/cidadaoPrimeiraClasse.js
+++ funcao/cidadaoPrimeiraClasse.js
@@ -1,6 +1,32 @@
 //High-order function
 //First-Class Object
 
-function fun1() {
-    
+function fun1() {}
+
+const fun2 = function () {}
+
+const array = [function (a, b) { return a + b }, fun1, fun2]
+
+console.log(array[0](2,3))
+
+const obj = {}
+obj.falar = function () { return 'Opa'}
+console.log(obj.falar())
+
+function run(fun){
+    fun()
 }
+
+const x = function () {
+    console.log('Executando...')
+}
+
+run(x)
+
+function soma(a, b){
+    return function(c){
+        console.log(a + b + c)
+    }
+}
+
+soma(3, 4)(1)
