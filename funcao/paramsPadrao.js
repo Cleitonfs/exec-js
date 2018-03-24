40d4605750d25ad052dab92aba4e7e2ece3ee12d
Genilton Cleiton <cleitonfs@yahoo.com>
2018-03-24 09:48:10 -0300

    outras estrategias de params padrao

--- funcao/paramsPadrao.js
+++ funcao/paramsPadrao.js
@@ -9,3 +9,17 @@ function soma1(a, b, c){
 console.log(soma1(10, 5))
 console.log(soma1())
 console.log(soma1(2,2,2))
+
+//passando parametros padrão com outras estratégias
+function soma2(a, b, c){
+    a = (a !== undefined) ? a : 1
+    b = 1 in arguments ? b : 1
+    c = isNaN(c) ? 1 : c
+    return a + b + c
+}
+console.log(soma2(10, 5))
+console.log(soma2())
+console.log(soma2(2,2,2))
+console.log(soma2(0,0,0))
+
+
