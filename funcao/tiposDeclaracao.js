b675f9c7f9d0138c7bab874611f0be93b7597a77
Genilton Cleiton <cleitonfs@yahoo.com>
2018-03-31 17:29:50 -0300

    como declarar funções

---
+++ funcao/tiposDeclaracao.js
@@ -0,0 +1,21 @@
+console.log(soma(10, 5))
+console.log(sub(10, 5))
+
+
+
+//function declaration
+function soma(x, y) {
+    return x + y
+}
+
+//fucntion expression
+const sub = function (x, y) {
+    return x - y
+}
+
+//named function expression
+const mult = function mult(x, y) {
+    return x * y
+}
+
+//
