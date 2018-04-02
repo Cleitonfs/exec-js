3e211c42e9079f5c5609f376bc69c6163412562e
Genilton Cleiton <cleitonfs@yahoo.com>
2018-04-01 22:35:53 -0300

    update

---
+++ objeto/objetoRevisao.js
@@ -0,0 +1,43 @@
+
+const produto = new Object
+produto.nome = 'Cadeira'
+produto['Marca do produto'] = 'Generica'
+produto.preco = 220
+
+console.log(produto)
+
+const carro = {
+    modelo: 'A5',
+    valor: 100000,
+    proprietario: {
+        nome: 'Raul',
+        idade: 56,
+        endereco: {
+            logradouro: 'Rua Anc',
+            numero: 23
+        }
+    }, 
+    condutores: [{
+        nome: 'Junior',
+        idade:19
+    }, {
+            nome: 'Ana',
+            idade: 45
+        }], 
+        calcularValorSeguro: function() {
+            //...
+        }
+}
+
+carro.proprietario.endereco.numero = 1000
+carro['proprietario']['endereco']['logradouro'] = 'Av. Gigante'
+console.log(carro)
+
+delete carro.condutores
+delete carro.proprietario.endereco
+delete carro.calcularValorSeguro
+
+
+
+
+
